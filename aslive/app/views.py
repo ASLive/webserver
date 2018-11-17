96297156fde1e19d0cd100ecb19a942104387d65
Nate Schultz <ngs32@drexel.edu>
2018-11-17 10:25:37 -0500

    removed index.html

--- aslive/app/views.py
+++ aslive/app/views.py
@@ -5,9 +5,6 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
-def index(request):
-    return render(request, 'app/index.html', {})
-
 def room(request, room_name):
     return render(request, 'app/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
