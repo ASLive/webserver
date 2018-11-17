0c73ecbe9a041ae1c0ef122b02504a2fee1a0dc3
Nate Schultz <ngs32@drexel.edu>
2018-11-17 11:30:08 -0500

    worked on translator

--- aslive/app/translate.py
+++ aslive/app/translate.py
@@ -7,13 +7,15 @@ class Translator():
     english_cache = []
 
     def __init__(self,consumer):
-        self.consumer = consumer
+        self.client = consumer
 
     def process(self,data):
         self.raw_data_cache.append(data)
         self.detect_asl_word()
 
     def detect_asl_word(self):
-        # detect asl words/gestures
-        detected = "word"
-        self.consumer.send(text_data=json.dumps({'translation': detected}))
+        last_word = self.raw_data_cache[-1]
+        if "asl" in last_word:
+            self.client.send(text_data=json.dumps(
+                {'translation': last_word}
+            ))
