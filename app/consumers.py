7c6eac2fe683af8134654c1dd9792c1353901513
Nate Schultz <ngs32@drexel.edu>
2018-11-27 21:19:07 -0500

    updated readme, cleaned consumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,28 +1,19 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+import uuid
 from . import translate
 
 class AslConsumer(WebsocketConsumer):
 
     def connect(self):
-        self.room_name = "" #self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'client_%s' % self.room_name
+        self.client_name = 'client_%s' % uuid.uuid4()
         self.translator = translate.Translator(self)
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
+        async_to_sync(self.channel_layer.group_add)(self.client_name,self.channel_name)
         self.accept()
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+        async_to_sync(self.channel_layer.group_discard)(self.client_name,self.channel_name)
 
     def receive(self, text_data):
         self.translator.process(json.loads(text_data)['input'])
--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,3 @@
 from django.conf.urls import url
 from . import consumers
-
 websocket_urlpatterns = [url(r'^$', consumers.AslConsumer),]
