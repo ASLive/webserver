605fdb74b0bb751e63e5c695e0fc3d06539531af
Evan Radcliffe <itces2@drexel.edu>
2019-02-12 19:19:01 -0500

    Separate video into frames

--- app/consumers.py
+++ app/consumers.py
@@ -1,9 +1,13 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+import sys
 import uuid
 from . import translate
 import base64
+from datetime import datetime
+import cv2
+from contextlib import closing
 
 class AslConsumer(WebsocketConsumer):
 
@@ -17,14 +21,28 @@ class AslConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)(self.client_name,self.channel_name)
 
     def receive(self, text_data):
+        print('receiving video @ ' + str(datetime.now().time()))
 
         # decode video data from base64
         # remove leading padding header (data:video/mp4;base64,)
+        # TODO: find a safer way to strip header
         decoded_string = base64.b64decode(text_data[22:])
 
         # write video data to mp4 file
         f = open('test.mp4','wb')
         f.write(decoded_string)
+        f.close()
+
+        vidcap = cv2.VideoCapture('test.mp4')
+        success, image = vidcap.read()
+        count = 0
+        success = True
+        while success:
+          cv2.imwrite("frames/frame_%d.jpg" % count, image)     # save frame as JPEG file
+          success, image = vidcap.read()
+          count += 1
+
+        print('stored file @ ' + str(datetime.now().time()))
 
         # print data for debugging
         # print("\n\n\n"+text_data+"\n\n\n");
