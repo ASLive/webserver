bcbb1992b8db72182ce13b49da2aed33206d1854
Nate Schultz <ngs32@drexel.edu>
2019-03-05 10:56:23 -0500

    cleaned up

--- app/translate.py
+++ app/translate.py
@@ -1,49 +1,39 @@
 from __future__ import print_function, unicode_literals
 import json
 import numpy as np
-
 import keras
 from keras.models import model_from_json
 import pickle
-
 import numpy as np
 import scipy.misc
-
+from hand3d.nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork
+from hand3d.utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3d
 import tensorflow as tf
 global graph, model
+
 graph = tf.get_default_graph()
+JSON_PATH = "./bin/model.json"
+WEIGHTS_PATH = "./bin/model.h5"
+CLASSES_PATH = "./bin/classes.pickle"
 
 def read_model():
-
-
-    JSON_PATH = "./bin/model.json"
-    WEIGHTS_PATH = "./bin/model.h5"
-    CLASSES_PATH = "./bin/classes.pickle"
-
-
-    # load json and create model
+    # load json into new model
     json_file = open(JSON_PATH, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
     loaded_model.load_weights(WEIGHTS_PATH)
-
+    # compile model
     loaded_model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'],)
     return loaded_model
 
 model = read_model()
+classes = pickle.load( open(CLASSES_PATH,"rb") )
 
 class Translator():
-
-    #TODO: clean up imports
-
-
-    JSON_PATH = "./bin/model.json"
-    WEIGHTS_PATH = "./bin/model.h5"
-    CLASSES_PATH = "./bin/classes.pickle"
 
     image_tf = None
     hand_side_tf = None
@@ -62,8 +52,6 @@ class Translator():
         self.client = consumer
 
         print('Initializing hand3d')
-        from hand3d.nets.ColorHandPose3DNetwork import ColorHandPose3DNetwork
-        from hand3d.utils.general import detect_keypoints, trafo_coords, plot_hand, plot_hand_3d
 
         # network input
         self.image_tf = tf.placeholder(tf.float32, shape=(1, 240, 320, 3))
@@ -82,47 +70,47 @@ class Translator():
 
     # TODO: bulk process frames?
     def image_to_hand(self, image):
+        """convert an image frame into hand vector with hand3d"""
+        # pre processing
         image_raw = scipy.misc.imresize(image, (240, 320))
         image_v = np.expand_dims((image_raw.astype('float') / 255.0) - 0.5, 0)
 
-        hand_scoremap_v, image_crop_v, scale_v, center_v,\
-        keypoints_scoremap_v, keypoint_coord3d_v = self.sess.run([self.hand_scoremap_tf, self.image_crop_tf, self.scale_tf, self.center_tf,
-                                                             self.keypoints_scoremap_tf, self.keypoint_coord3d_tf],
-                                                            feed_dict={self.image_tf: image_v})
+        # prediction
+        input = [self.hand_scoremap_tf, self.image_crop_tf,
+                self.scale_tf, self.center_tf,
+                self.keypoints_scoremap_tf, self.keypoint_coord3d_tf]
+        keypoint_coord3d_v = self.sess.run(input,feed_dict={self.image_tf: image_v})[-1]
 
+        # post processing
         keypoint_coord3d_v = np.squeeze(keypoint_coord3d_v)
-        return keypoint_coord3d_v
+        hand_vector = np.expand_dims(keypoint_coord3d_v,0)
+        
+        return hand_vector
 
-    def read_classes(self):
-        return pickle.load( open(self.CLASSES_PATH,"rb") )
-
-    def predict(self, hand):
+    def letter_predict(self, hand):
+        """convert hand vector data to asl letter"""
         ret = None
         with graph.as_default():
             ret = model.predict(hand)
 
         return ret
 
-
-    raw_data_cache = []
-    asl_word_cache = []
-    english_cache = []
-
     def process(self,video_frames):
-        self.detect_asl_letter(video_frames)
-
-    def detect_asl_letter(self,video_frames):
-        hand = self.image_to_hand(video_frames[0])
-        hand = (np.expand_dims(hand,0))
-        predictions = self.predict(hand=hand)
-        classes = self.read_classes()
-        for eval in predictions:
-            print(eval)
-            predicted_label = np.argmax(eval)
-            predicted_letter = classes[predicted_label]
-            # TODO: have threshold of confidence
-            # to not spam client every frame
-            print(predicted_letter)
-            self.client.send(text_data=json.dumps(
-                {'translation': predicted_letter}
-            ))
+        """
+        for each video frame:
+            - extract the hand vector data
+            - predict the asl letter
+        """
+        for frame in video_frames:
+            hand = self.image_to_hand(frame)
+            predictions = self.letter_predict(hand)
+            for eval in predictions:
+                predicted_label = np.argmax(eval)
+                predicted_letter = classes[predicted_label]
+                # TODO: have threshold of confidence
+                # to not spam client every frame
+                print(eval)
+                print(predicted_letter)
+                self.client.send(text_data=json.dumps(
+                    {'translation': predicted_letter}
+                ))
